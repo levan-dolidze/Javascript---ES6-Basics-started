85a0dfc0f5cd182ad2890f861cf357c28a8a308e
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2020-08-23 14:55:39 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,280 @@
+// old
+// var number=10;
+// var number=5;
+
+
+// var name="levani"
+// function logName(name){
+//     var newName=name || "levani";
+//     console.log(newName);
+//     const welcomeMessage="hello "+name+" you are "+age+" years old"
+    
+//     console.log(welcomeMessage);
+// }
+
+// logName();
+
+// var obj ={
+//     name:"dato",
+//     age:25,
+//     favoriteNumbers:[1,2,3]
+// };
+
+// var name=obj.name;
+// var age=obj.age;
+// var favoriteNumbers=obj.favoriteNumbers;
+
+// console.log(obj.name,obj.age,obj.favoriteNumbers)
+
+
+// for (var i = 0; i < 5; i++) {
+//     console.log(i);
+    
+// }
+
+// console.log("eror", i)
+
+
+
+//es6
+// const,let
+
+// const number =10;
+// const number=10;
+
+// console.log(number)
+// let test="lel";
+// test="pele";
+
+// console.log(test);
+
+//arrow function
+// const logAge =(age=10,name="leva")=>{
+//     const welcomeMessage=`hello ${name} you are ${age} years old`
+    
+//     console.log(welcomeMessage);
+
+// }
+// logAge(20);
+
+
+
+// const obj2 ={
+//     name:"dato",
+//     age:25,
+//     favoriteNumbers:[1,2,3]
+// };
+
+// const{name,age,favoriteNumbers} =obj2;
+// console.log(name,age,favoriteNumbers);
+
+// for (let index = 0; index < 5; index++) {
+//    console.log(index);
+// }
+// console.log(index)
+
+
+
+// const ES6 =(age1=10,name1="leva", id1=22222)=>{
+//     const welcomeMessage=`hello ${name1} you are ${age1} years old your ID is: ${id1}`
+    
+//     const objtask ={
+//         name:"dato",
+//         age:25,
+//         id:55555
+//     };
+//     const {age,name,id} = objtask;
+//     console.log(name1,age1,id1);
+//     console.log(welcomeMessage);
+   
+
+// }
+//     ES6(50,name,11111);
+
+
+//     const array=[1,5,7];
+//     for (let index = 0; index <5; index++) {
+//     array.push(index);
+        
+//     }
+
+    
+    // console.log(array)
+    
+    // array.push(8)
+    // console.log(array)
+
+
+//abrunebs ert konkretul elements
+//    const evenNum= array.find((element)=>{
+// return element % 2 === 0;
+//     });
+//     console.log(evenNum)
+
+//daabrunos kvela luwi elementi
+
+// const num = array.filter((element)=>{
+//     return element%2===0;
+// });
+
+// console.log(num)
+
+// const stringArr =['a','b','c'];
+// stringArr.filter((element)=>{
+//     return element ==="c"
+// })
+// console.log(stringArr);
+
+
+//old
+// var number=10;
+// var number=15;
+
+// var name="levani";
+// function logName(name,age){
+//     var newName=name||"ll";
+//     var welcomeMessage="hello "+ name+" you are "+age + " years old";
+//     console.log(welcomeMessage);
+// }
+
+// logName("levan",12);
+
+// var obj ={
+//     name1:"ani",
+//     age1:25,
+//     favNumbers:[1,5,6]
+// };
+
+// var name =obj.name1;
+// var age=obj.age1;
+// var favNumbers=obj.favNumbers
+
+// console.log(obj.name1,obj.age1,obj.favNumbers)
+
+
+// for (var index = 0; index < 5; index++) {
+    
+//     console.log(index)
+// }
+
+// console.log("oooo", index)
+
+// //es6
+// //const an let
+
+// const number=10;
+// // const number=15;
+// let test="java";
+// test="python"
+
+//arrow functions
+// const logAge =(age=10,name="sofo")=>{
+//     const welcomeMessage=`hello ${name}  you are ${age} years old`
+//     console.log(welcomeMessage)
+// }
+// logAge(5)
+
+
+
+// const te =(levan,dolidze,weli)=>{
+// const dd=`ravaxar ${levan} ${dolidze} xo xar magra? ${weli} s xar xo ?`
+//     console.log(dd)
+// }
+// te("levan ","dolidze ",20)
+
+
+
+// const objnew ={
+//     name1:"ani",
+//     age1:25,
+//     favNumbers:[1,5,6]
+// };
+
+// const{name1,age1,favNumbers} =objnew
+// console.log(name1,age1,favNumbers)
+
+
+
+// for (let index = 0; index < 5; index++) {
+//    console.log(index)
+    
+// }
+// console.log("ppp", index)
+
+
+
+// const task =(a1="lika",b1="shevchenko",c1=30)=>{
+//     const objnew ={
+//         a:"ani",
+//         b:25,
+//         c:[1,5,6]
+//     };
+//     const{a,b,c} =objnew
+//    console.log(a1,b1,c1)
+    
+
+//     const dd=`ravaxar ${a1} ${b1} xo xar magra? ${c1} sheni sakvareli cifrebia ?`
+//         console.log(dd)
+//     }
+//     task("levan")
+
+
+
+//     const array =[1,7,9,10];
+//     for (let index = 0; index < 11; index++) {
+//         array.push(index)
+        
+//     }
+//     console.log(array)
+//     const eve = array.find((element)=>{
+//     return element % 2===0;
+//     });
+//     console.log(eve)
+    
+    
+
+    const number=prompt(`ent your numbers`)
+    if(number%2===0){
+        document.write(`the number ${number} is an even number`)
+    }
+    else{
+        document.write(`the number: ${number} is an odd number`)
+    }
+    
+
+const array=[1,2,3,4,5,6];
+for (let index = 0; index < 10; index++) {
+   
+    array.push(index)
+    console.log(array)
+    
+}
+console.log(array)
+
+
+
+
+
+
+
+//     const envenn = array.filter((element)=>{
+// return element%2===1;
+//     })
+//     console.log(envenn);
+    
+//     array.push(9/array.length)
+//     console.log(array)
+
+
+
+
+
+
+
+
+
+// const eve1=array.find((element)=>{
+// return element %2===0;
+// });
+// console.log(eve1);
+
